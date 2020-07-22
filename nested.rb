039caba9b2552aa8f9eb5f22be9a5a8a94b79d42
Ciara Picou <ciarapicou@att.net>
2020-07-22 17:55:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,10 +60,7 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
+
 # return the entire updated hash
 
 	programmer_hash = 
@@ -81,7 +78,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
 end
 
 def changing_alan
