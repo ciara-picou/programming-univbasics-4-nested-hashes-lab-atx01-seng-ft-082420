bdaae3c7cf94e836b4ff5b42a1664323806acfd6
Ciara Picou <ciarapicou@att.net>
2020-07-22 17:59:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,7 @@ programmer_hash
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
+  
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -124,5 +122,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+programmer_hash
 end
